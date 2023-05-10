3a86d9e5aee6a438bf087735b9d5993a60a4bdeb
Sheldon Pierce <Pierce55@icloud.com>
2023-05-09 20:03:31 -0700

    Completed lab for the day

--- app/page.js
+++ app/page.js
@@ -1,113 +1,80 @@
-import Image from 'next/image'
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <>
+        <header className="text-7xl font-semibold p-10 bg-green-500 mx-auto text-left h-40"> Cookie Stand Admin </header>
+        <main className='h=2/3 bg-white h-4/5'>
+            <div className="flex items-center justify-center bg-white">
+                <div className='w-3/5 mt-14 mx-20 rounded-[20px] bg-green-300 mx-15 h-max'>
+                    <form className="align-content-center rounded-lg w-fit">
+                        <div className="font-bold text-4xl text-center p-8">
+                            <text className="p-8">
+                                Create Cookie Stand
+                            </text>
+                        </div>
+                        <div className="mx-3 font-semibold flex items-stretch">
+                            <text className='text-3xl p-2'>
+                                Location
+                            </text>
+                            <input
+                                type="text"
+                                className="w-full mx-5"
+                                placeholder='Seattle'
+                            />
+                        </div>
+                        <div className="flex justify-between mt-8 mb-6">
+                            <div className="m-6">
+                                <div className='mx-auto text-center mb-2'>
+                                    <text className="text-3xl text-center font-semibold">
+                                        Minimum Customers per Hour
+                                    </text>
+                                </div>
+                                <div>
+                                    <input
+                                        type="text"
+                                        className= 'p-2 w-full'
+                                        placeholder='4'
+                                    />
+                                </div>
+                            </div >
+                            <div className="m-6">
+                                <div className='mx-auto text-center mb-2'>
+                                    <text className="text-3xl text-center font-semibold">
+                                        Maximum Customers per Hour
+                                    </text>
+                                </div>
+                                <div>
+                                    <input type="text" className='p-2 w-full' placeholder='50' />
+                                </div>
+                            </div>
+                            <div className="m-6">
+                                <div className='mx-auto text-center mb-2'>
+                                    <text className="text-3xl text-center font-semibold">
+                                        Average Cookies per Sale
+                                    </text>
+                                </div>
+                                <div>
+                                    <input type="text" className='p-2 w-full' placeholder='4.7' />
+                                </div>
+                            </div>
+                            <div role='button' className="bg-green-500 w-4/12 mr-8 flex items-center justify-center" >
+                                <text className="text-3xl mx-auto text-center font-semibold">
+                                    Create
+                                </text>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div className="m-8 text-center h-40 flex items-center justify-center bg-white">
+                    <text className="text-center text-3xl font-semibold">
+                         Report Table Coming Soon...
+                    </text>
+                </div>
+        </main>
+        <footer className="bg-green-500 px-10 py-5 font-semibold h-36 flex items-center justify-left text-3xl"> &copy 2023</footer>
+      </>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   )
 }
