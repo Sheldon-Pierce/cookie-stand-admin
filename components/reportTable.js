34b54521ef742b78d5c43f730ab3dfe8b5e9f8ff
Sheldon Pierce <Pierce55@icloud.com>
2023-05-15 14:33:53 -0700

    Vercel things

--- components/reportTable.js
+++ components/reportTable.js
@@ -39,8 +39,8 @@ export default function ReportTable ({listOfReports}) {
                         <th>
                             Location
                         </th>
-                        {hours.map(hour => (
-                            <th key={hour}>
+                        {hours.map((hour, idx) => (
+                            <th key={idx}>
                                 {hour}
                             </th>
                         ))}
@@ -50,25 +50,26 @@ export default function ReportTable ({listOfReports}) {
                     </tr>
                 </thead>
                 <tbody className='border border-gray-400 even:bg-green-200 odd:bg-green-300'>
-                    {resources.map(location => (
-                        <tr className='border border-gray-400 even:bg-green-200 odd:bg-green-300'>
+                    {resources.map((location, idx) => (
+                        <tr key={location.id} className='border border-gray-400 even:bg-green-200 odd:bg-green-300'>
                             <td className='text-center' key={location.id}>
+
                                 {location.location}
                                 <span onClick={() => deleteResource(location.id)}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round"
+                                    <svg key={idx} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round"
         d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"/>
 </svg>
                                 </span>
                             </td>
-                            {location.hourly_sales.map(list =>(
-                                <td className='text-center border border-gray-400' key='list'>
+                            {location.hourly_sales.map((list, idx) =>(
+                                <td className='text-center border border-gray-400' key={idx}>
                                     {list}
                                 </td>
                                 ))}
                                 <td className='text-center'>
-                                    {location.hourly_sales.reduce((acc, list) => acc + list, 0)}
+                                    {location.hourly_sales.reduce((acc, lst) => acc + lst, 0)}
                                 </td>
                         </tr>
                     ))}
@@ -78,8 +79,8 @@ export default function ReportTable ({listOfReports}) {
                         <th className='text-center border border-gray-400 bg-green-500'>
                             Totals
                         </th>
-                            {footerTotals().map(totals => (
-                                <td className='text-center'>
+                            {footerTotals().map((totals, idx) => (
+                                <td className='text-center' key={idx}>
                                     {totals}
                                 </td>
                             ))}
